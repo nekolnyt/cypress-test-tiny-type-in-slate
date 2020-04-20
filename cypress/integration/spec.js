123f03eefa2f4c82e9f02da0e214e3e01bf51325
Tomas Nekolny <tomas.nekolny@productboard.com>
2020-04-20 15:26:36 +0200

    add type in slate test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.slatejs.org/examples/richtext')
+
+    const testString = 'Hello world';
+
+    cy.get('[data-slate-editor="true"]').type(`${testString}{enter}`);
+
+    cy.contains('[data-slate-string="true"]', testString).should('be.visible');
   })
 })
